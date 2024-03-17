f7a577eaa5d4a8ed05f21e9ea25644dc6d44ce64
Alessandro Trigolo <alessandrotrigolo@gmail.com>
2024-03-17 21:12:29 +0100

    improved KMP-search.cpp
    added also input.txt and output.txt files

--- algorithms/pattern-matching/KMP-search.cpp
+++ algorithms/pattern-matching/KMP-search.cpp
@@ -1,45 +1,110 @@
 /* @imAlessas */
 
 #include <bits/stdc++.h>
+#include "../generate-data/generator.cpp"
 
 using namespace std;
 
 // function definition
-void fill_table(int[], const string&);
-int* KMP_search(const string&, const string&, int[]);
+vector<int> get_oracle(const string&);
+
+vector<int> KMP_search(const string&, const string&, const vector<int>&);
+
 
 int main() {
+    // generates data
+    generate_DNA_sequence(100000);
+
+
+    // uses file to address input and output
+    ifstream input("input.txt");
+    ofstream output("output.txt");
+
+    if (!input.is_open() || !output.is_open()) {
+        cout << "Error opening files." << endl;
+        return 1;
+    }
+
     string sequence, pattern;
+    getline(input, sequence);
+    // getline(input, pattern);
+    pattern = "ATGTG";
 
-    cin >> sequence >> pattern;
+    // obtains oracle table
+    vector<int> oracle = get_oracle(pattern);
 
-    int lookup[pattern.size()] = {0};
+    auto start = chrono::high_resolution_clock::now();
+    
+    // performs KMP-search
+    vector<int> positions = KMP_search(sequence, pattern, oracle);
 
-    fill_table(lookup, pattern);
+    // calculates duration
+    auto stop = chrono::high_resolution_clock::now();
+    auto duration = chrono::duration_cast<chrono::milliseconds>(stop - start);
 
+    // ouputs duration and matches
+    cout << endl << "KMP-search:  " << duration.count() << " ms" << endl;
+    cout << "             " << positions.size() << " match(es)" << endl << endl;
 
+    // writes the indexes
+    for (int i : positions)
+        output << i << endl;
 
+    input.close();
+    output.close();
+
+    // opens output file
+    system("start output.txt");
 
     return 0;
 } // main
 
 
-void fill_table(int lookup[], const string& pattern){
-    int i = 0;
 
-    for(int j = 1; j < pattern.size(); j++){
-        while(i > 0 && pattern[i] != pattern[j])
-            i = lookup[i - 1];
+
+// implementation of Knuth–Morris–Pratt algorithm
+vector<int> KMP_search(const string& sequence, const string& pattern, const vector<int>& oracle) {
+    int j = 0, k = 0;
+    vector<int> positions;
+
+    while (j < sequence.size()) {
+        if (pattern[k] == sequence[j]) {
+            j++;
+            k++;
+
+            if (k == pattern.size()) {
+                positions.push_back(j - k);
+                k = oracle[k - 1];
+            }
+
+        } else {
+            if(k)
+                k = oracle[k - 1];
+            else
+                j++; 
+        }
+    }
+
+    return positions;
+} //  KMP_search
+
+
+
+
+// oracle (lookup table) implementation
+vector<int> get_oracle(const string& pattern) {
+    int i = 0;
+    vector<int> oracle(pattern.size());
+
+    for (int j = 1; j < pattern.size(); j++) {
+        while (i > 0 && pattern[i] != pattern[j])
+            i = oracle[i - 1];
         
-        if(pattern[i] == pattern[j])
+        if (pattern[i] == pattern[j])
             i++;
         
-        lookup[j] = i;
+        oracle[j] = i;
     }
-} // fill_table
 
-
-int* KMP_search(const string& sequence, const string& pattern, int[] lookup){
-
-
-} //  KMP_search
+    return oracle;
+} // get_oracle
